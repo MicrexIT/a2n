67de10e6456f9ca59eedb7d12a16a0d5b99b5024
Michele Rexha <rexha.michele@gmail.com>
2022-05-25 18:27:51 +0200

    fix typo

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_UR
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
 const SUPABASE_PUBLIC_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_KEY
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY)
