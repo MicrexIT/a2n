82613467b293d6be3cbb95e286e1d943b266d820
Michele Rexha <rexha.michele@gmail.com>
2022-05-25 18:03:30 +0200

    add env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = "https://mpytemfbpkrgsikwydxv.supabase.co"
-const SUPABASE_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXRlbWZicGtyZ3Npa3d5ZHh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1NDg0MDIsImV4cCI6MTk2NjEyNDQwMn0.K5pjQ8_3mWZFM7ZderuGuXXqLgqQUMmiF90SUcyNNhs"
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_UR
+const SUPABASE_PUBLIC_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_KEY
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY)
 export default supabase
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,9 +11,27 @@ const config = {
 		})
 	],
 
-	kit: {
-		adapter: adapter()
-	}
-};
+	// kit: {
+	// 	adapter: adapter()
+	// }
 
-export default config;
+	kit: {
+		// default options are shown
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
+	}
+}
+
+export default config
